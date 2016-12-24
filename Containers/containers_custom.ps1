210ed76dbdd8c35ca8f8f71824c6213d3d617bf4
Kevin Booth <kbooth@rms.com>
2016-12-24 13:24:57 -0500

    removing unnecessary container scripts

--- Containers/containers_custom.ps1
+++
@@ -1,9 +0,0 @@
-﻿
-$references = ('Microsoft.Containers.PowerShell.Cmdlets')
-
-foreach($ref in $references) {
-    $assembly = [System.Reflection.Assembly]::LoadWithPartialName($ref);
-    $mod = Import-Module  -Scope Global -Assembly $assembly -PassThru -Force
-    $mod = Import-Module  -Scope Global -Assembly $assembly -PassThru -Force -Function 'Install-ContainerOSImage'
-
-}
